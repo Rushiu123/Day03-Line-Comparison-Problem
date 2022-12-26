fe66e998038e0f7ededf148e143405f012a2c33e
Rushiu123 <rushikesh.ubalkar@gmail.com>
2022-12-26 21:26:36 +0530

    Day8LinecomparisonAndMethod

--- Program.cs
+++ Program.cs
@@ -1,67 +1,58 @@
 ﻿namespace Day03_LineComparisonProblem
 {
-    internal class Program
+    class Program
     {
-        public static object LineEqualityCheck { get; private set; }
-
-        static void Main(string[] args, object lineEqualityCheck)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Enter 0 for Welcome Message program\nEnter 1 for Distance between line UC1\nEnter 2 for Line Equality check");
-            Console.WriteLine("Enter 0 for Welcome Message program\nEnter 1 for Distance between line UC1\nEnter 2 for Line Equality check\nEnter 3 for Comparison Between Line");
-            int option = Convert.ToInt32(Console.ReadLine());
-            switch (option)
+            Console.WriteLine("Welcome to Line Comparison Problem!");
+            //Length of Line - UC1
+            Console.WriteLine("Enter X1 co-ordinate : ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y1 co-ordinate : ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X2 co-ordinate : ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y2 co-ordinate : ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double lengthOfFirstLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of 1st Line is : " + lengthOfFirstLine);
+            Console.WriteLine("Enter X3 co-ordinate : ");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y3 co-ordinate : ");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter X4 co-ordinate : ");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Y4 co-ordinate : ");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+            double lengthOfSecondLine = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+
+            Console.WriteLine("Length of 2nd Line is : " + lengthOfSecondLine);
+            //UC2 & UC3
+            if (lengthOfFirstLine == lengthOfSecondLine)
+                //UC4 - CompareTo Method
+                int returnVal = lengthOfFirstLine.CompareTo(lengthOfSecondLine);
+            Console.WriteLine(returnVal);
+            //UC2 - Line Equality
+            if (returnVal == 0)
             {
-                case 0:
-                    Console.WriteLine("Welcomw! to Line Comparison Pragram");
-                    break;
-                case 1:
-                    Console.WriteLine("Please enter X1 and Y1 coordinates");
-                    var X1 = Convert.ToDouble(Console.ReadLine());
-                    var Y1 = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Please enter X2 and Y2 coordinates");
-                    var X2 = Convert.ToDouble(Console.ReadLine());
-                    var Y2 = Convert.ToDouble(Console.ReadLine());
-                    var finalResult = DistanceBetweenLine.Distance(X1, X2, Y1, Y2);
-                    Console.WriteLine("Distance between coordinates {0},{1} and {2},{3} is {4}", X1, Y1, X2, Y2, finalResult);
-                    break;
-                case 2:
-                    Console.WriteLine("Please enter X3 and Y3 coordinates");
-                    var X3 = Convert.ToDouble(Console.ReadLine());
-                    var Y3 = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Please enter X4 and Y4 coordinates");
-                    var X4 = Convert.ToDouble(Console.ReadLine());
-                    var Y4 = Convert.ToDouble(Console.ReadLine());
-
-                    Console.WriteLine("Please enter X5 and Y5 coordinates");
-                    var X5 = Convert.ToDouble(Console.ReadLine());
-                    var Y5 = Convert.ToDouble(Console.ReadLine());
-
-                    Console.WriteLine("Please enter X6 and Y6 coordinates");
-                    var X6 = Convert.ToDouble(Console.ReadLine());
-                    var Y6 = Convert.ToDouble(Console.ReadLine());
-                    lineEqualityCheck.Equals(X3, Y3, X4, Y4, X5, Y5, X6, Y6);
-                    break;
-                case 3:
-                    Console.WriteLine("Please enter X7 and Y7 coordinates");
-                    var X7 = Convert.ToDouble(Console.ReadLine());
-                    var Y7 = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Please enter X8 and Y8 coordinates");
-                    var X8 = Convert.ToDouble(Console.ReadLine());
-                    var Y8 = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Please enter X9 and Y9 coordinates");
-                    var X9 = Convert.ToDouble(Console.ReadLine());
-                    var Y9 = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Please enter X10 and Y10 coordinates");
-                    var X10 = Convert.ToDouble(Console.ReadLine());
-                    var Y10 = Convert.ToDouble(Console.ReadLine());
-                    ComparisonBetweenLine.CompareOfLine(X7, Y7, X8, Y8, X9, Y9, X10, Y10);
-                    break;
-                default:
-                    Console.WriteLine("Please enter right option");
-                    break;
+                Console.WriteLine("Lines are Equal");
             }
+            else if (lengthOfFirstLine >= lengthOfSecondLine)
+            //UC3 - Line Comparision
+            else if (returnVal > 0)
+            {
+              Console.WriteLine("1st Line is greater than 2nd Line");
+              Console.WriteLine("Line 1 is greater than Line 2");
+            }
+             else
+             {
+               Console.WriteLine("1st Line is smaller than 2nd Line");
+               Console.WriteLine("Line 1 is smaller than Line 2");
+             }
+
         }
     }
-     
 }
+     
+
 
